4885e8d511fead09d5c0c24dbf722fdb332795a6
Artur Baraszkiewicz <artur.baraszkiewicz@euvic.pl>
2018-10-28 18:11:02 +0100

    CSV reader

--- CsvReader/Program.cs
+++ CsvReader/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using CsvReader.Model;
+using System.Linq;
 using System.Collections.Generic;
 using CsvReader.Settings;
 using CsvHelper;
@@ -14,7 +15,12 @@ namespace CsvReader
         public List<MailModel> Read() {
             List<MailModel> users;
             using (var streamReader = File.OpenText(Config.Path)) {
-                
+                //TODO: change namespace
+                var reader = new CsvHelper.CsvReader(streamReader);
+                reader.Configuration.Delimiter = Config.Delimiter;
+                users = reader.GetRecord<MailModel>().Skip(_currentIndex).Take(Config.Count).ToList();
+                _currentIndex += Config.Count;
+
             }
             return null;
         }
--- CsvReader/Settings/Config.cs
+++ CsvReader/Settings/Config.cs
@@ -10,5 +10,8 @@ namespace CsvReader.Settings
         public const int Count = 100;
         //file with addresses
         public const string Path = @"";
+        //delimiter
+        public const string Delimiter = ";";
+
     }
 }
